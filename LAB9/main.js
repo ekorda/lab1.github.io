5d687d96a8ff0ed6589f173744874ca0b9d2d728
ekorda <ekorda@miu.edu>
2022-04-10 18:54:44 -0500

    8 9

--- LAB9/main.js
+++ LAB9/main.js
@@ -1,100 +1,55 @@
-
-//question 1
-
-class student {
-constructor(firstName , lastName , grades) {
- this.firstName = firstName;
-  this.lastName = lastName; 
-  this.grades = grades;
-  }
-  
-   inputNewGrade (newGarde) {
-    this.grades.push(newGarde);
-    }
-    computeAverageGrade() {
-    return this.grades.reduce((sum, current, index, array) => sum + current / array.length, 0)
-    }
-
+class LinkedList {
+constructor(arr){
+ if(arr){
+ arr.forEach(elem => this.add(elem));
+ }
 }
-
-
-//question 2
-function Student(firstName, lastName, grades = []) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.grades = grades;
+ add(element) {
+ if (this.value === undefined) {
+ this.value = element;
+ this.next = null;
+ } else {
+ let current = this;
+ while (current.next) {
+ current = current.next;
+ }
+ current.next = { value: element, next: null };
+ }
+ }
+ remove(element) {
+ var current = this;
+ var prev = null;
+ while (current) {
+ if (current.value === element) {
+ if (prev == null) {
+ this.value = current.next.value;
+ this.next = current.next.next;
+ } else {
+ prev.next = current.next;
+ }
+ return true;
+ }
+ prev = current;
+ current = current.next;
+ }
+ return false;
+ }
+ printHelper(list, result) {
+ if (list.next == null) {
+ result += list.value;
+ return result;
+ }
+ result += list.value + ',';
+ return this.printHelper(list.next, result);
+ }
+ print() {
+ let result = 'LinkedList{';
+ result = this.printHelper(this, result);
+ result += '}';
+ console.log(result);
+ }
 }
-
-Student.prototype.inputNewGrade = function (newGarde) {
-    this.grades.push(newGarde);
-}
-Student.prototype.computeAverageGrade = function () {
-    return this.grades.reduce((sum, current, index, array) => sum + current / array.length, 0)
-}
-
-//question 3
-Array.prototype.mysort = function () {
-    let arr = this;
-    let len = arr.length;
-    for (let i = len - 1; i >= 0; i--) {
-        for (let j = 1; j <= i; j++) {
-            if (arr[j - 1] > arr[j]) {
-                let temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    return arr;
-}
-console.log([7, 5, 2, 4, 3, 9].mysort());
-
-//Question 4 : 
-let linkedlist = {};
-linkedlist.add = function (element) {
-    if (this.value === undefined) {
-        this.value = element;
-        this.next = null;
-    } else {
-        let current = this;
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = { value: element, next: null };
-    }
-}
-linkedlist.remove = function (element) {
-    var current = this;
-    var prev = null;
-    while (current) {
-        if (current.value === element) {
-            if (prev == null) {
-                this.value = current.next.value;
-                this.next = current.next.next;
-            } else {
-                prev.next = current.next;
-            }
-            return true;
-        }
-        prev = current;
-        current = current.next;
-    }
-    return false;
-}
-linkedlist.printHelper = function (list, result) {
-    if (list.next == null) {
-        result += list.value;
-        return result;
-    }
-    result += list.value + ',';
-    return this.printHelper(list.next, result);
-}
-linkedlist.print = function () {
-    let result = 'LinkedList{';
-    result = this.printHelper(this, result);
-    result += '}';
-    console.log(result);
-}
+let linkedlist = new LinkedList();
 linkedlist.add(1);
 linkedlist.add(2);
 linkedlist.add(3);
@@ -102,3 +57,61 @@ linkedlist.print(); //in the console, you should see: LinkedList{1,2,3}
 linkedlist.remove(3);
 linkedlist.print(); //in the console, you should see: LinkedList{1,3}
 
+
+
+/////////////
+
+
+
+function Question(qid, answer) {
+this.qid = qid;
+this.answer = answer;
+}
+Question.prototype.checkAnswer = function(answer) {
+return this.answer === answer;
+}
+function Student(studentId) {
+this.studentId = studentId;
+this.answers = [];
+}
+Student.prototype.addAnswer = function(question) {
+this.answers.push(question);
+}
+function Quiz(questions, students) {
+this.questions = new Map();
+questions.forEach(q => this.questions.set(q.qid, q.answer));
+this.students = students;
+}
+Quiz.prototype.scoreStudentBySid = function(studentId) {
+const student = this.students.filter(s => s.studentId === studentId)[0];
+return student.answers.reduce((sum, currentQuestion) => {
+if (currentQuestion.checkAnswer(this.questions.get(currentQuestion.qid)))
+{
+sum = sum + 1;
+}
+return sum;
+}, 0);
+}
+Quiz.prototype.getAverageScore = function() {
+return this.students.reduce((accmulator, student, index, array) => {
+return accmulator + this.scoreStudentBySid(student.studentId) / array.len
+gth;
+}, 0);
+}
+const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
+const student1 = new Student(10);
+student1.addAnswer(new Question(2, 'a'));
+student1.addAnswer(new Question(3, 'b'));
+student1.addAnswer(new Question(1, 'b'));
+const student2 = new Student(11);
+student2.addAnswer(new Question(3, 'b'));
+student2.addAnswer(new Question(2, 'a'));
+student2.addAnswer(new Question(1, 'd'));
+const students = [student1, student2];
+const quiz = new Quiz(questions, students);
+let scoreforStudent10 = quiz.scoreStudentBySid(10);
+console.log(scoreforStudent10);
+let scoreforStudent11 = quiz.scoreStudentBySid(11);
+console.log(scoreforStudent11);
+let average = quiz.getAverageScore();
+console.log(average);
